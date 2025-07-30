9a8cf94b44c7da2ca0afdcd26a5a855d24657857
pashtetx <vadimplej43@gmail.com>
2025-07-30 22:24:40 +0200

    media redirect

--- core/rtc.py
+++ core/rtc.py
@@ -1,6 +1,25 @@
-from aiortc.contrib.media import MediaBlackhole
+from aiortc.contrib.media import PlayerStreamTrack
 
 
 class MediaRedirect:
 
-    def __init__() -> 
+    def __init__(self, track: PlayerStreamTrack) -> None:
+        self.track = track
+        self.__audio = PlayerStreamTrack(self, kind="audio")
+
+    @property
+    def audio(self) -> PlayerStreamTrack:
+        return self.__audio
+
+    async def start(self) -> None:
+        await self.__run_track(self.track)
+
+    async def __run_track(self, track: PlayerStreamTrack) -> None:
+        while True:
+            try:
+                frame = await track.recv()
+            except Exception as e:
+                print("EXCEPTION", e)
+            await self.__audio._queue.put(frame)
+
+    
